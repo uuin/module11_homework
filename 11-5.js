c24eb263a972cb8b150abcbfd5bc36e90362ece5
uuin <uuin11@gmail.com>
2024-08-14 22:36:41 +0500

    Add files via upload

--- 11-5.js
+++ 11-5.js
@@ -1,13 +1,4 @@
-function countInOrderEverySecond(number1, number2) {
-    let progress = number1;
-
-    let intervalId = setInterval(function () {
-        if (progress <= number2) {
-            console.log(progress);
-            progress++;
-        }
-        else clearInterval(intervalId);
-    }, 1000);
+const pow = (x, n) => {
+    return x ** n;
 }
-
-countInOrderEverySecond(5, 15);
+console.log(pow(10, 2));
